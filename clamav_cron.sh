f27144bee5afa9d42598752595549211f527861e
Jeff Shepherd <hummdis@gmail.com>
2020-02-17 15:15:57 -0700

    v1.0.1
    -Changed some paths to better match the Shell Scan process.
    -Added the option to automatically quarantine or not. Default is to automatically quarantine.

--- clamav_cron.sh
+++ clamav_cron.sh
@@ -21,11 +21,18 @@
 # Make sure you include the quotes!!
 ####
 
+# If you don't want to automatically quarantine anything found, change this to 'false'.
+QUARANTINE='true'
+
+# Don't make edits below unless you know what you're doing.
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+VERSION=1.0.1
 TIMESTAMP=$(date +%Y-%m-%H_%k:%M:%S)
 MAIN_LOG="/var/log/clamav/clamav_summary-${TIMESTAMP}.txt"
 EMAIL_MSG="The scheduled ClamAV scan is complete. Review the attached log summary for information.  This scans log file name is scanlog-${TIMESTAMP}.txt."
 EMAIL_FROM="clamav@$(hostname)"
 EMAIL_TO=${1}
+DATE=$(date +%Y%m%d%H%M)
 
 # Update ClamAV databases
 echo "Updating ClamAV databases..."
@@ -34,26 +41,32 @@ freshclam > /dev/null 2>&1
 for USER in $(awk '{print $2}' /etc/trueuserdomains)
 do
 	# Make the directory for logging if it doesn't exist.
-	if [[ ! -e /home/$USER/scanlogs/ ]]
+	if [[ ! -e /home/$USER/quarantine/ ]]
 	then
-		mkdir -p /home/$USER/scanlogs/
+		mkdir -p /home/$USER/quarantine
 	fi
 	
+	mkdir -p /home/$USER/quarantine/quarantine_$DATE
+	
 	echo -e "\nScanning $USER..."
-	clamscan -ri --exclude-dir="^/sys" --log=/home/$USER/scanlogs/scanlog-$TIMESTAMP.txt /home/$USER/
+	if [[ $QUARANTINE == 'true' ]]; then
+		clamscan -ri --move=/home/$USER/quarantine/quarantine_$DATE/ --log=/home/$USER/quarantine/quarantine_$DATE/scanlog --max-dir-recursion=1000 --exclude="zip$" --exclude="tar.gz$" /home/$USER/
+	else
+		clamscan -ri --exclude-dir="^/sys" --log=/home/$USER/quarantine/quarantine_$DATE/scanlog --max-dir-recursion=1000 --exclude="zip$" --exclude="tar.gz$" /home/$USER/
+	fi
 	
 	# Part 2, cron job to chown contents of folder to ensure cPanel readability
 	echo "Setting $USER scanlog permissions..."
-	chown -R $USER:$USER /home/$USER/scanlogs/* 
+	chown -R $USER:$USER /home/$USER/quarantine/quarantine_$DATE/* 
 	
 	# Part 3, cron job to prone out log files older than 14 days
 	echo "Cleaning up $USER scanlogs folder..."
-	find -P /home/$USER/scanlogs/. -type f -mtime '+14' -exec rm {} \;
+	find -P /home/$USER/quarantine/quarantine_$DATE/. -type f -mtime '+14' -exec rm {} \;
 
 	# Now, compile the summaries into a single log file to email to the desired email address.
 	# The extra 'echo' statements are for formatting the email due to limitations with echo and the 'mail' command.
 	echo -e "\n\nSummary for user ${USER}:\n" >> $MAIN_LOG
-	tail -9 /home/$USER/scanlogs/scanlog-$TIMESTAMP.txt >> $MAIN_LOG
+	tail -9 /home/$USER/quarantine/quarantine_$DATE/scanlog >> $MAIN_LOG
 
 done
 
